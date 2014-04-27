b02eda81d9e1a0a754cbd97eb08093119de0dbce
WhatsARanjit <whatsaranjit@gmail.com>
2014-04-27 14:21:54 +0000

    Added puppet-lint gem install

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,6 +2,10 @@ class vim::install {
   package { [ "vim-common", "vim-enhanced" ]:
     ensure => installed,
   }
+  package { 'puppet-lint':
+    ensure   => installed,
+    provider => 'gem',
+  }
 }
 
 
