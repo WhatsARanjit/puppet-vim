44ca9bc98b8a4cb33c7a6f69f2f4044e1088908b
WhatsARanjit <whatsaranjit@gmail.com>
2016-05-08 22:40:13 -0400

    oops

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,2 @@
 require 'rspec-puppet-utils'
-require 'hiera/backend/eyaml'
 require 'puppetlabs_spec_helper/module_spec_helper'
-
-RSpec.configure do |c|
- c.hiera_config = 'spec/fixtures/hiera/hiera.yaml'
-end
